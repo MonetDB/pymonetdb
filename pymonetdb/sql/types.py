2e923f8e7d603314e6d53fdbfcbd1131104e3888
PedroTadim <PedroTadim@users.noreply.github.com>
2016-09-16 14:59:24 +0100

    Added the UUID, JSON and Geometry types support. Cleaned unused types. (#9)
    Added support for UUID, JSON and Geometry types, with the last one just converting to strings so far. Cleaned unused types.
    
    * Fixed types list for compability reasons

--- pymonetdb/sql/types.py
+++ pymonetdb/sql/types.py
@@ -11,38 +11,43 @@ http://www.monetdb.org/Documentation/Manuals/SQLreference/Datatypes
 
 CHAR = 'char'                      # (L) character string with length L
 VARCHAR = 'varchar'                # (L) string with atmost length L
-CLOB = 'clob'
-BLOB = 'blob'
-DECIMAL = 'decimal'                # (P,S)
+CLOB = 'clob'                      # string with no limit on size
+BLOB = 'blob'                      # binary data
+
+TINYINT = 'tinyint'                # 8 bit integer
 SMALLINT = 'smallint'              # 16 bit integer
 INT = 'int'                        # 32 bit integer
 BIGINT = 'bigint'                  # 64 bit integer
+HUGEINT = 'hugeint'                # 128 bit integer
 SERIAL = 'serial'                  # special 64 bit integer sequence generator
 REAL = 'real'                      # 32 bit floating point
 DOUBLE = 'double'                  # 64 bit floating point
-BOOLEAN = 'boolean'
-DATE = 'date'
+DECIMAL = 'decimal'                # (P,S)
+BOOLEAN = 'boolean'                # boolean value
+
+DATE = 'date'                      # (D) date
 TIME = 'time'                      # (T) time of day
 TIMESTAMP = 'timestamp'            # (T) date concatenated with unique time
-INTERVAL = 'interval'              # (Q) a temporal interval
+MONTH_INTERVAL = 'month_interval'  # number of months
+SEC_INTERVAL = 'sec_interval'      # number of seconds
 
-MONTH_INTERVAL = 'month_interval'
-SEC_INTERVAL = 'sec_interval'
-WRD = 'wrd'
-TINYINT = 'tinyint'
-
-URL = 'url'
-INET = 'inet'
+URL = 'url'                        # url
+INET = 'inet'                      # ipv4 address
+UUID = 'uuid'                      # uuid
+JSON = 'json'                      # JSON string
+GEOMETRY = 'geometry'              # geometry string
+GEOMETRYA = 'geometrya'            # geometry string
 
 # Not on the website:
 SHORTINT = 'shortint'
 MEDIUMINT = 'mediumint'
 LONGINT = 'longint'
-HUGEINT = 'hugeint'
-FLOAT = 'float'
-TIMESTAMPTZ = 'timestamptz'
-TIMETZ = 'timetz'
-
+OID = 'oid'
+WRD = 'wrd'
+FLOAT = 'float'                    # 64 bit floating point
+TIMESTAMPTZ = 'timestamptz'        # with timezone
+TIMETZ = 'timetz'                  # with timezone
+INTERVAL = 'interval'              # (Q) a temporal interval
 
 # full names and aliases, spaces are replaced with underscores
 CHARACTER = CHAR
