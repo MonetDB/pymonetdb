b09b038c4490070ae837eed0ce7ab7c20001af15
Gijs Molenaar <gijs@pythonic.nl>
2016-11-01 09:51:42 +0100

    prepare for 1.0.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 setup(name='pymonetdb',
       version=__version__,
