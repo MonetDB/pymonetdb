163f2a865a7f653a486c6344c287918b2525765c
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2019-08-16 13:03:33 +0200

    Initialize mapi connection at the ProfilerConnection constuctor

--- pymonetdb/profiler.py
+++ pymonetdb/profiler.py
@@ -1,3 +1,8 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0.  If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright 1997 - July 2008 CWI, August 2008 - 2016 MonetDB B.V.
 """
 This implements a connection to the profiler.
 """
@@ -11,14 +16,13 @@ class ProfilerConnection(object):
     """
 
     def __init__(self):
-        self._mapi = None
+        self._mapi = mapi.Connection()
         self._heartbeat = 0
         self._buffer = ""
         self._objects = list()
 
     def connect(self, database, username, password, hostname=None, port=50000, heartbeat=0):
         self._heartbeat = heartbeat
-        self._mapi = mapi.Connection()
         self._mapi.connect(database, username, password, "mal", hostname, port)
         self._mapi.cmd("profiler.setheartbeat(%d);\n" % heartbeat)
         self._mapi.cmd("profiler.openstream(3);\n")
