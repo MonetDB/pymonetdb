855e8ad71ccf31777a03a54c9a806af5147c9b74
Gijs Molenaar <gijs@pythonic.nl>
2019-09-17 16:17:10 +0200

    prepare for 1.2.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
-__version__ = '1.2.0'
+__version__ = '1.2.1'
 
 setup(name='pymonetdb',
       version=__version__,
