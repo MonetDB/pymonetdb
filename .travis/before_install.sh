d6f2b4066058168acd3cb8e2f667a47d3b29631a
Gijs Molenaar <gijs@pythonic.nl>
2019-09-17 09:03:33 +0200

    bla

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -9,15 +9,16 @@ sudo apt-get update -q
 sudo apt-get install -qy monetdb5-sql monetdb-client
 
 # start database
-sudo -u monetdb mkdir -m 700 /var/lib/monetddb
-sudo -u monetdb /usr/bin/monetdbd create /var/lib/monetdb
-sudo -u monetdb /usr/bin/monetdbd start /var/lib/monetdb
+sudo mkdir -m 770 /var/lib/monetdb
+sudo chown -R monetdb.monetdb /var/lib/monetdb
+sudo -u monetdb monetdbd create /var/lib/monetdb
+sudo -u monetdb monetdbd start /var/lib/monetdb
+sudo -u monetdb monetdbd set control=yes /var/lib/monetdb
+sudo -u monetdb monetdbd set passphrase=testdb /var/lib/monetdb
 
 # set up test database
 sudo monetdb create demo
 sudo monetdb release demo
-sudo monetdbd set control=yes /var/lib/monetdb
-sudo monetdbd set passphrase=testdb /var/lib/monetdb
 
 # install python test requirements
 pip install -r test/requirements.txt
