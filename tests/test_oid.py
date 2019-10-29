30af7153b339144834b58bc4908a06dadc554d34
Gijs Molenaar <gijs@pythonic.nl>
2019-10-29 13:18:22 +0100

    fix oid support

---
+++ tests/test_oid.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from pymonetdb import connect
+from tests.util import test_args
+
+
+class TestOid(TestCase):
+    def setUp(self):
+        self.connection = connect(autocommit=False, **test_args)
+        self.cursor = self.connection.cursor()
+
+    def test_oid(self):
+        q = "select tag from sys.queue()"
+        self.cursor.execute(q)
+        rows = self.cursor.fetchall()
