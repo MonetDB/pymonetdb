b12cda4745fab5e9d5b9c122840d215e73cfd1f0
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2020-02-18 15:03:09 +0100

    Take into account the profiler.openstream interface change

--- pymonetdb/profiler.py
+++ pymonetdb/profiler.py
@@ -25,7 +25,12 @@ class ProfilerConnection(object):
         self._heartbeat = heartbeat
         self._mapi.connect(database, username, password, "mal", hostname, port)
         self._mapi.cmd("profiler.setheartbeat(%d);\n" % heartbeat)
-        self._mapi.cmd("profiler.openstream(3);\n")
+        try:
+            self._mapi.cmd("profiler.openstream();\n")
+        except:
+            # We might be talking to an older version of MonetDB. Try connecting
+            # the old way.
+            self._mapi.cmd("profiler.openstream(3);\n")
 
     def read_object(self):
         self._buffer = self._mapi._getblock()
