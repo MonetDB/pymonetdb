daf9285adea563f17f99d957a6c4b1b8d69b0847
Gijs Molenaar <gijs@pythonic.nl>
2020-02-18 14:39:21 +0100

    upgrade travis env

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,25 +1,19 @@
 #!/bin/bash -ve
 
-# install monetdb
-sudo apt-get install software-properties-common
-sudo apt-get update -q
-sudo sh -c "echo 'deb http://dev.monetdb.org/downloads/deb/ xenial monetdb' > /etc/apt/sources.list.d/monetdb.list"
-wget --output-document=- http://dev.monetdb.org/downloads/MonetDB-GPG-KEY | sudo apt-key add -
-sudo apt-get update -q
-sudo apt-get install -qy monetdb5-sql monetdb-client
+export DBFARM=/var/lib/monetdb
 
 # start database
-sudo mkdir -p -m 770 /var/lib/monetdb
-sudo chown -R monetdb.monetdb /var/lib/monetdb
-sudo -u monetdb monetdbd create /var/lib/monetdb
-sudo -u monetdb monetdbd start /var/lib/monetdb
-sudo -u monetdb monetdbd set control=yes /var/lib/monetdb
-sudo -u monetdb monetdbd set passphrase=testdb /var/lib/monetdb
+sudo mkdir -p -m 770 ${DBFARM}
+sudo chown -R monetdb.monetdb ${DBFARM}
+sudo -u monetdb monetdbd create ${DBFARM}
+sudo -u monetdb monetdbd set control=yes ${DBFARM}
+sudo -u monetdb monetdbd set passphrase=testdb ${DBFARM}
+sudo -u monetdb monetdbd start ${DBFARM}
 
 # set up test database
 sudo -u monetdb monetdb create demo
-sudo -u monetdb monetdb set embedpy=true demo
-sudo -u monetdb monetdb release demo
+sudo -u monetdb monetdb set embedpy3=true demo
+sudo -u monetdb monetdb start demo
 
 # install python test requirements
 pip install -r tests/requirements.txt
