0e3c5ac2aaede1321bf95803d4456afd902dfa39
Joeri van Ruth <joeri.van.ruth@monetdbsolutions.com>
2022-03-16 14:02:17 +0100

    More tests

--- tests/test_filetransfer.py
+++ tests/test_filetransfer.py
@@ -1,18 +1,24 @@
+from io import StringIO
+from pathlib import Path
+from shutil import copyfileobj
+from tempfile import mkdtemp
 from typing import Optional
-from unittest import TestCase
+from unittest import TestCase, skip, skipUnless
 
-from unittest import skip
 
 from pymonetdb import connect, Error as MonetError
 from pymonetdb.exceptions import OperationalError, ProgrammingError
-from pymonetdb.mapi import Upload, Uploader
-from tests.util import test_args
+from pymonetdb.mapi import Download, Downloader, Upload, Uploader
+from tests.util import test_args, test_full
+
+
+class MyException(Exception):
+    pass
 
 
 class TestUploader(Uploader):
     rows: int = 5_000
     error_at: Optional[int] = None
-    error_msg: Optional[str] = None
     chunkSize: int = 10_000
     force_binary: bool = False
     forget_to_return_after_error: bool = False
@@ -38,7 +44,7 @@ class TestUploader(Uploader):
             w = upload.text_writer()
             for i in iter:
                 if i == self.error_at:
-                    raise Exception(f"Oops {i}")
+                    raise MyException(f"Oops {i}")
                 if upload.is_cancelled() and not self.ignore_cancel:
                     self.cancelled_at = i
                     break
@@ -48,7 +54,7 @@ class TestUploader(Uploader):
             w = upload.binary_writer()
             for i in iter:
                 if i == self.error_at:
-                    raise Exception(f"Oops {i}")
+                    raise MyException(f"Oops {i}")
                 if upload.is_cancelled() and not self.ignore_cancel:
                     self.cancelled_at = i
                     break
@@ -56,14 +62,57 @@ class TestUploader(Uploader):
                 w.write(bytes(s, 'ascii'))
 
 
-class TestUpload(TestCase):
+class TestDownloader(Downloader):
+    lines: Optional[int] = None
+    error_at_line: Optional[int] = None
+    refuse: Optional[str] = None
+    forget_to_return_after_refusal: bool = False
+    buffer: StringIO
+
+    def __init__(self):
+        self.buffer = StringIO()
+
+    def handle(self, download: Download, filename: str, text_mode: bool):
+        if self.refuse:
+            download.send_error(self.refuse)
+            if not self.forget_to_return_after_refusal:
+                return
+        if self.lines is None:
+            copyfileobj(download.text_reader(), self.buffer)
+        else:
+            i = 0
+            for line in download.text_reader():
+                if i >= self.lines >= 0:
+                    break
+                elif self.error_at_line is not None and self.error_at_line == i:
+                    raise MyException("oopsie")
+                i += 1
+                self.buffer.write(line)
+
+    def get(self):
+        return self.buffer.getvalue()
+
+
+class TestFileTransfer(TestCase):
     first = True
+    tmpdir: Optional[Path] = None
+
+    def file(self, filename):
+        if not self.tmpdir:
+            self.tmpdir = Path(mkdtemp())
+        return self.tmpdir.joinpath(filename)
+
+    def open(self, filename, mode, **kwargs):
+        fullname = self.file(filename)
+        return open(fullname, mode, **kwargs)
 
     def setUp(self):
         super().setUp()
         self.conn = conn = connect(**test_args)
         self.uploader = TestUploader()
         conn.set_uploader(self.uploader)
+        self.downloader = TestDownloader()
+        conn.set_downloader(self.downloader)
 
         self.cursor = c = self.conn.cursor()
         if self.first:
@@ -82,6 +131,9 @@ class TestUpload(TestCase):
         except MonetError:
             pass
         super().tearDown()
+
+    def fill_foo(self, nrows):
+        self.execute("INSERT INTO foo(i) SELECT * FROM sys.generate_series(1, %s + 1)", [nrows])
 
     def execute(self, *args, **kwargs):
         return self.cursor.execute(*args, **kwargs)
@@ -123,7 +175,7 @@ class TestUpload(TestCase):
         with self.assertRaises(OperationalError):
             self.execute("COPY INTO foo FROM 'xfoo' ON CLIENT")
 
-    def test_offset0(self):
+    def test_upload_offset0(self):
         # OFFSET 0 and OFFSET 1 behave the same, they do nothing
         self.uploader.chunkSize = 100
         self.uploader.rows = 100
@@ -131,7 +183,7 @@ class TestUpload(TestCase):
         self.execute("SELECT MIN(i) AS mi, MAX(i) AS ma FROM foo")
         self.expect([(1, 100)])
 
-    def test_offset1(self):
+    def test_upload_offset1(self):
         # OFFSET 0 and OFFSET 1 behave the same, they do nothing
         self.uploader.chunkSize = 100
         self.uploader.rows = 100
@@ -139,17 +191,120 @@ class TestUpload(TestCase):
         self.execute("SELECT MIN(i) AS mi, MAX(i) AS ma FROM foo")
         self.expect([(1, 100)])
 
-    def test_offset5(self):
+    def test_upload_offset5(self):
         self.uploader.chunkSize = 100
         self.uploader.rows = 100
         self.execute("COPY OFFSET 5 INTO foo FROM 'foo' ON CLIENT")
         self.execute("SELECT MIN(i) AS mi, MAX(i) AS ma FROM foo")
         self.expect([(5, 100)])
 
-    def test_server_cancels(self):
+    def test_server_cancels_upload(self):
         # self.uploader.chunkSize = 100
         self.execute("COPY 10 RECORDS INTO foo FROM 'foo' ON CLIENT")
         self.assertGreater(self.uploader.cancelled_at, 0)
         self.execute("SELECT COUNT(*) FROM foo")
         self.expect1(10)
 
+    def test_download_refused(self):
+        self.downloader.refuse = 'no thanks'
+        with self.assertRaises(OperationalError):
+            self.execute("COPY (SELECT * FROM foo) INTO 'foo' ON CLIENT")
+        # connection still alive
+        self.conn.rollback()
+        self.execute("SELECT 42")
+        self.expect1(42)
+
+    def test_download(self):
+        self.fill_foo(5)
+        self.execute("COPY (SELECT * FROM foo) INTO 'foo' ON CLIENT")
+        self.assertEqual("1\n2\n3\n4\n5\n", self.downloader.get())
+
+    def test_download_lines(self):
+        self.downloader.lines = -1
+        self.fill_foo(5)
+        self.execute("COPY (SELECT * FROM foo) INTO 'foo' ON CLIENT")
+        self.assertEqual("1\n2\n3\n4\n5\n", self.downloader.get())
+
+    def test_download_stop_reading_halfway(self):
+        self.fill_foo(10000)
+        self.downloader.lines = 10
+        self.execute("COPY (SELECT * FROM foo) INTO 'foo' ON CLIENT")
+        self.assertEqual("1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n", self.downloader.get())
+        # connection still alive
+        self.execute("SELECT 42")
+        self.expect1(42)
+
+    @skipUnless(test_full, "full test disabled")
+    def test_large_upload(self):
+        n = 1_000_000
+        self.uploader.rows = n
+        self.uploader.chunkSize = 1024 * 1024
+        self.execute("COPY INTO foo FROM 'foo' ON CLIENT")
+        self.assertIsNone(self.uploader.cancelled_at)
+        self.execute("SELECT COUNT(*) FROM foo")
+        self.expect1(n)
+
+    @skipUnless(test_full, "full test disabled")
+    def test_large_download(self):
+        n = 1_000_000
+        self.fill_foo(n)
+        self.execute("COPY (SELECT * FROM foo) INTO 'banana' ON CLIENT")
+        content = self.downloader.get()
+        nlines = len(content.splitlines())
+        self.assertEqual(n, nlines)
+
+    def test_upload_native_text_file(self):
+        self.upload_file('native.csv', {}, True)
+
+    def test_upload_unix_text_file(self):
+        self.upload_file('unix.csv', dict(newline="\n"), True)
+
+    def test_upload_dos_text_file(self):
+        self.upload_file('dos.csv', dict(newline="\r\n"), True)
+
+    def test_upload_unix_binary_file(self):
+        self.upload_file('unix.csv', dict(newline="\n"), False)
+
+    def upload_file(self, filename, write_opts, read_text):
+        n = 1000
+        f = self.open(filename, 'w', **write_opts)
+        for i in range(n):
+            print(f"{i}|Únïçøðε{i}", file=f)
+        f.close()
+        testcase = self
+
+        class CustomUploader(Uploader):
+            def handle(self, upload: Upload, filename: str, text_mode: bool, skip_amount: int):
+                if read_text:
+                    f = testcase.open(filename, 'r')
+                    w = upload.text_writer()
+                else:
+                    f = testcase.open(filename, 'rb')
+                    w = upload.binary_writer()
+                copyfileobj(f, w)
+                f.close()
+
+        self.conn.set_uploader(CustomUploader())
+        self.execute(f"COPY INTO foo2 FROM '{filename}' ON CLIENT")
+        self.execute("SELECT t FROM foo2 where i = 999")
+        self.expect1("Únïçøðε999")
+
+    @skip("post-exception connection handling needs to be fixed")
+    # see also java test test_FailUploadLate2 which I don't understand at all
+    def test_fail_upload_late(self):
+        self.uploader.error_at = 99
+        with self.assertRaises(MyException):
+            self.execute("COPY INTO foo FROM 'foo' ON CLIENT")
+        self.execute("SELECT COUNT(*) FROM foo")
+        self.expect1(self.uploader.rows)
+
+    @skip("post-exception connection handling needs to be fixed")
+    def test_fail_download_late(self):
+        self.fill_foo(5000)
+        self.downloader.lines = 6000
+        self.downloader.error_at_line = 4000
+        with self.assertRaises(MyException):
+            self.execute("COPY (SELECT * FROM foo) INTO 'foo' ON CLIENT")
+        self.conn.rollback()
+        self.execute("SELECT 42")
+        self.expect1(42)
